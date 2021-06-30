7d559b21447e632e75766edb5f09ff2a2b0ce753
minchi kang <minchikang@gmail.com>
2021-06-30 18:05:35 +0900

    2021-06-30 불필요삭제

--- routes/tourApi.js
+++ routes/tourApi.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const urlencode = require('urlencode'); 
 const dotenv = require('dotenv');
 dotenv.config();
 
